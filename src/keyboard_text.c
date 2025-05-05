54ac59aed9437d26aa96b04f3a64f9867fc6d2e1
ofir286@gmail.com <ofir286@gmail.com>
2025-05-05 21:24:54 +0300

    fixed naming keyboard

--- src/keyboard_text.c
+++ src/keyboard_text.c
@@ -1,31 +1,46 @@
 #include "global.h"
 
 // Easy Chat keyboard
-const u8 gText_EasyChatKeyboard_ABCDEFothers[] = _("{CLEAR 11}A{CLEAR 6}B{CLEAR 6}C{CLEAR 26}D{CLEAR 6}E{CLEAR 6}F{CLEAR 26}others");
-const u8 gText_EasyChatKeyboard_GHIJKL[] = _("{CLEAR 11}G{CLEAR 6}H{CLEAR 6}I{CLEAR 26}J{CLEAR 6}K{CLEAR 6}L");
-const u8 gText_EasyChatKeyboard_MNOPQRS[] = _("{CLEAR 11}M{CLEAR 6}N{CLEAR 6}O{CLEAR 26}P{CLEAR 6}Q{CLEAR 6}R{CLEAR 6}S{CLEAR 26} ");
-const u8 gText_EasyChatKeyboard_TUVWXYZ[] = _("{CLEAR 11}T{CLEAR 6}U{CLEAR 6}V{CLEAR 26}W{CLEAR 6}X{CLEAR 6}Y{CLEAR 6}Z{CLEAR 26} ");
+const u8 gText_EasyChatKeyboard_ABCDEFothers[] = _("{CLEAR 11}א{CLEAR 6}ב{CLEAR 6}ג{CLEAR 26}ד{CLEAR 6}ה{CLEAR 6}ו{CLEAR 26}others");
+const u8 gText_EasyChatKeyboard_GHIJKL[] = _("{CLEAR 11}ז{CLEAR 6}ח{CLEAR 6}ט{CLEAR 26}י{CLEAR 6}כ{CLEAR 6}ל{CLEAR 6}מ");
+const u8 gText_EasyChatKeyboard_MNOPQRS[] = _("{CLEAR 11}נ{CLEAR 6}ס{CLEAR 6}ע{CLEAR 26}פ{CLEAR 6}צ{CLEAR 6}ק{CLEAR 6}ר{CLEAR 26} ");
+const u8 gText_EasyChatKeyboard_TUVWXYZ[] = _("{CLEAR 11}ש{CLEAR 6}ת{CLEAR 6}ן{CLEAR 26}ם{CLEAR 6}ץ{CLEAR 6}ף{CLEAR 6}ך{CLEAR 26} ");
 
 // Naming Screen keyboard
 const u8 gText_NamingScreenKeyboard_abcdef[] = _("{CLEAR 11}a{CLEAR 6}b{CLEAR 6}c{CLEAR 26}d{CLEAR 6}e{CLEAR 6}f{CLEAR 6} {CLEAR 26}.");
 const u8 gText_NamingScreenKeyboard_ghijkl[] = _("{CLEAR 11}g{CLEAR 6}h{CLEAR 7}i{CLEAR 27}j{CLEAR 6}k{CLEAR 6}l{CLEAR 7} {CLEAR 26},");
 const u8 gText_NamingScreenKeyboard_mnopqrs[] = _("{CLEAR 11}m{CLEAR 6}n{CLEAR 7}o{CLEAR 26}p{CLEAR 6}q{CLEAR 7}r{CLEAR 6}s{CLEAR 27} ");
 const u8 gText_NamingScreenKeyboard_tuvwxyz[] = _("{CLEAR 12}t{CLEAR 6}u{CLEAR 6}v{CLEAR 26}w{CLEAR 6}x{CLEAR 6}y{CLEAR 6}z{CLEAR 26} ");
+// Ofir changed here
+/*
 const u8 gText_NamingScreenKeyboard_ABCDEF[] = _("{CLEAR 11}A{CLEAR 6}B{CLEAR 6}C{CLEAR 26}D{CLEAR 6}E{CLEAR 6}F{CLEAR 6} {CLEAR 26}.");
 const u8 gText_NamingScreenKeyboard_GHIJKL[] = _("{CLEAR 11}G{CLEAR 6}H{CLEAR 6}I{CLEAR 26}J{CLEAR 6}K{CLEAR 6}L{CLEAR 6} {CLEAR 26},");
 const u8 gText_NamingScreenKeyboard_MNOPQRS[] = _("{CLEAR 11}M{CLEAR 6}N{CLEAR 6}O{CLEAR 26}P{CLEAR 6}Q{CLEAR 6}R{CLEAR 6}S{CLEAR 26} ");
 const u8 gText_NamingScreenKeyboard_TUVWXYZ[] = _("{CLEAR 11}T{CLEAR 6}U{CLEAR 6}V{CLEAR 26}W{CLEAR 6}X{CLEAR 6}Y{CLEAR 6}Z{CLEAR 26} ");
+ */
+const u8 gText_NamingScreenKeyboard_ABCDEF[] = _("{CLEAR 11}א{CLEAR 6}ב{CLEAR 5}ג{CLEAR 25}ד{CLEAR 5}ה{CLEAR 6}ו{CLEAR 6} {CLEAR 26}.");
+const u8 gText_NamingScreenKeyboard_GHIJKL[] = _("{CLEAR 11}ז{CLEAR 6}ח{CLEAR 5}ט{CLEAR 25}י{CLEAR 8}כ{CLEAR 6}ל{CLEAR 5}מ{CLEAR 26},");
+const u8 gText_NamingScreenKeyboard_MNOPQRS[] = _("{CLEAR 11}נ{CLEAR 7}ס{CLEAR 6}ע{CLEAR 24}פ{CLEAR 6}צ{CLEAR 5}ק{CLEAR 7}ר{CLEAR 26} ");
+const u8 gText_NamingScreenKeyboard_TUVWXYZ[] = _("{CLEAR 11}ש{CLEAR 6}ת{CLEAR 5}ן{CLEAR 26}ם{CLEAR 6}ץ{CLEAR 6}ף{CLEAR 6}ך{CLEAR 26} ");
 const u8 gText_NamingScreenKeyboard_01234[] = _("{CLEAR 11}0{CLEAR 16}1{CLEAR 16}2{CLEAR 16}3{CLEAR 16}4{CLEAR 16} ");
 const u8 gText_NamingScreenKeyboard_56789[] = _("{CLEAR 11}5{CLEAR 16}6{CLEAR 16}7{CLEAR 16}8{CLEAR 16}9{CLEAR 16} ");
 const u8 gText_NamingScreenKeyboard_Symbols1[] = _("{CLEAR 11}!{CLEAR 16}?{CLEAR 16}♂{CLEAR 16}♀{CLEAR 16}/{CLEAR 16}-");
 const u8 gText_NamingScreenKeyboard_Symbols2[] = _("{CLEAR 11}…{CLEAR 16}“{CLEAR 16}”{CLEAR 18}‘{CLEAR 18}'{CLEAR 18} ");
 
 // Union Room Chat keyboard
+// Ofir changed here
+/*
 const u8 gText_UnionRoomChatKeyboard_ABCDE[] = _("ABCDE");
 const u8 gText_UnionRoomChatKeyboard_FGHIJ[] = _("FGHIJ");
 const u8 gText_UnionRoomChatKeyboard_KLMNO[] = _("KLMNO");
 const u8 gText_UnionRoomChatKeyboard_PQRST[] = _("PQRST");
 const u8 gText_UnionRoomChatKeyboard_UVWXY[] = _("UVWXY");
+*/
+const u8 gText_UnionRoomChatKeyboard_ABCDE[] = _("אבגדה");
+const u8 gText_UnionRoomChatKeyboard_FGHIJ[] = _("וזחטי");
+const u8 gText_UnionRoomChatKeyboard_KLMNO[] = _("כלמנס");
+const u8 gText_UnionRoomChatKeyboard_PQRST[] = _("עפצקר");
+const u8 gText_UnionRoomChatKeyboard_UVWXY[] = _("שתןםץףך");
 const u8 gText_UnionRoomChatKeyboard_Z[] = _("Z    ");
 const u8 gText_UnionRoomChatKeyboard_01234Upper[] = _("01234");
 const u8 gText_UnionRoomChatKeyboard_56789Upper[] = _("56789");
